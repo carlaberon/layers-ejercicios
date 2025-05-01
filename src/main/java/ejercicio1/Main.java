56f60b9da763e3f164006aafc1d82db523f6b79d
Carla Berón <alracnoreb@gmail.com>
2025-05-01 14:17:56 -0300

    paquetes

---
+++ src/main/java/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ejercicio1;
+
+import java.awt.*;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new AgregarParticipante();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        });
+    }
+}
